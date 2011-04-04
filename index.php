e6bf48f4ab8da675d1b1fa6b61bde07fcf1f527e
tanaki <nipigelet@gmail.com>
2011-04-04 23:34:40 +0100

    setup html + css rotation

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Marie Laurent - Interactive and Graphic Designer</title>
+
+        <link rel="stylesheet" href="css/reset.css" type="text/css" />
+        <link rel="stylesheet" href="css/default.css" type="text/css" />
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js" type="text/javascript"></script>
+
+        <script src="js/lib/jquery.imgpreload.min.js" type="text/javascript"></script>
+        <script src="js/ML/init.js" type="text/javascript"></script>
+    </head>
+
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <div id="header-left">
+                    <a id="logo" href="/"><img src="/img/site/misc/icon_logo_24.png" alt="Marie Laurent Logo"/></a>
+                    <ul>
+                        <li id="resume">
+                            <a href="#">My Resume</a>
+                        </li>
+                        <li id="mail">
+                            <a href="#"><img src="/img/site/misc/icon_mail_24.png" alt="Mail mawielauwent[at]gmail[dot]com" /></a>
+                        </li>
+                        <li id="linkedin">
+                            <a href="http://www.linkedin.com/pub/marie-laurent/23/364/b13/en"><img src="/img/site/misc/icon_linkedin_24.png" alt="http://www.linkedin.com/pub/marie-laurent/23/364/b13/en" /></a>
+                        </li>
+                        <li id="twitter">
+                            <a href="http://twitter.com/mawiemawie"><img src="/img/site/misc/icon_twitter_24.png" alt="http://twitter.com/mawiemawie" /></a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="header-right">
+                    <ul id="menu">
+                        <li id="work">
+                            <a href="#">Work</a>
+                            <span>Lorem Ipsum</span>
+                        </li>
+                        <li id="short-bio">
+                            <a href="#">Short Bio</a>
+                            <span>Lorem Ipsum</span>
+                        </li>
+                        <li id="skills">
+                            <a href="#">Skills</a>
+                            <span>Lorem Ipsum</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div id="content">
+                <ul id="content-top">
+                    <li>
+                        <a class="first" href="/link1"><img src="/img/projects/thumbs/howbama_24.png" alt="Projet Yummy" /></a>
+                    </li>
+                    <li>
+                        <a href="/link2"><img src="/img/projects/thumbs/howbama_24.png" alt="Projet Yummy" /></a>
+                    </li>
+                    <li>
+                        <a href="/link2"><img src="/img/projects/thumbs/howbama_24.png" alt="Projet Yummy" /></a>
+                    </li>
+                    <li>
+                        <a href="/link2"><img src="/img/projects/thumbs/howbama_24.png" alt="Projet Yummy" /></a>
+                    </li>
+                    <li>
+                        <a href="/link2"><img src="/img/projects/thumbs/howbama_24.png" alt="Projet Yummy" /></a>
+                    </li>
+                    <li>
+                        <a href="/link2"><img src="/img/projects/thumbs/howbama_24.png" alt="Projet Yummy" /></a>
+                    </li>
+                    <li>
+                        <a href="/link2"><img src="/img/projects/thumbs/howbama_24.png" alt="Projet Yummy" /></a>
+                    </li>
+                </ul>
+                <ul id="content-bottom">
+                    <li>
+                        <a class="first" href="#"><img src="/img/projects/thumbs/howbama_24.png" alt="Projet Yummy" /></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="/img/projects/thumbs/howbama_24.png" alt="Projet Yummy" /></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="/img/projects/thumbs/howbama_24.png" alt="Projet Yummy" /></a>
+                    </li>
+                </ul>
+            </div>
+            <!--
+            <div id="footer">
+            </div>
+            -->
+        </div>
+    </body>
+</html>
---
+++ js/ML/init.js
@@ -0,0 +1,7 @@
+$(window).ready(function(){
+
+    // Resize ul
+    $("#content-top").css("width", $("#content-top li").length * 250);
+    $("#content-bottom").css("width", $("#content-bottom li").length * 250);
+
+});
